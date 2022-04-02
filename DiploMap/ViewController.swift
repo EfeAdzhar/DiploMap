de74c3645277ae42ef7e7270b94ad4b5b993553e
EfeAdzhar <efeadzhar@gmail.com>
2022-04-02 22:11:50 +0200

    Initial Commit

---
+++ DiploMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiploMap
+//
+//  Created by Efe on 02.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
